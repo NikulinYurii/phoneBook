4fc0c5a4474fc834eda0a4376e1e0574e0ed31ab
Your Name <you@example.com>
2017-07-12 16:11:31 +0300

    add validation class

---
+++ src/main/java/app/validation/Validation.java
@@ -0,0 +1,7 @@
+package app.validation;
+
+/**
+ * Created by yurii on 12.07.17.
+ */
+public interface Valid {
+}
---
+++ src/main/java/app/validation/ValidationImpl.java
@@ -0,0 +1,14 @@
+package app.validation;
+
+/**
+ * Created by yurii on 12.07.17.
+ */
+public class Validation {
+
+    public boolean validLogin(String login){
+
+            
+
+        return null;
+    }
+}
