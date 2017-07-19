e3a589c878566feab0fcd2f9ee56bfe1bb4dba9a
Your Name <you@example.com>
2017-07-19 17:38:00 +0300

    add user page

---
+++ src/main/java/app/controller/UserPageController.java
@@ -0,0 +1,7 @@
+package app.controller;
+
+/**
+ * Created by yurii on 19.07.17.
+ */
+public class UserPageController {
+}
---
+++ src/main/java/app/dto/LoginDTO.java
@@ -0,0 +1,7 @@
+package app.dto;
+
+/**
+ * Created by yurii on 19.07.17.
+ */
+public class LoginDTo {
+}
